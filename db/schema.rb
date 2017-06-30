b33ff5409d78aaf02a45df0610dc87ca09e55f8f
Jenyfer <jenyferpham@gmail.com>
2017-06-30 20:42:42 +0000

    added price table

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20170629035602) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "color"
-    t.string   "price"
+    t.decimal  "price"
   end
 
 end
