6c7cd80aadaa4abbc1c7d57721d75316aa7f012b
Jenyfer <jenyferpham@gmail.com>
2017-08-13 03:59:14 +0000

    created comment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base  #Originally was ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
     has_many :orders
+    has_many :comments
 end
