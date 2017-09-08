2c56d177b871f1e2d114dc95bc987c3d159d4cc0
Jenyfer <jenyferpham@gmail.com>
2017-09-08 00:03:43 +0000

    active job and advanced action cable

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,8 +3,11 @@ class Comment < ActiveRecord::Base #Originally was ApplicationRecord
   validates :user, presence: true
   validates :product, presence: true
   validates :rating, numericality: { only_integer: true }
+  
   belongs_to :user
   belongs_to :product
   
-   scope :rating_desc, -> { order(rating: :desc) }
+  scope :rating_desc, -> { order(rating: :desc) }
+   
+  after_create_commit { CommentUpdateJob.perform_later(self, @user) } 
 end
