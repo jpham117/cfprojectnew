977fd5967c40b3bd8254ff9d44865cb359194abb
Jenyfer <jenyferpham@gmail.com>
2017-09-12 19:23:34 +0000

    fixed user mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'sales@bikeberlin.com'
+  default from: 'jenyferpham@gmail.com'
   layout 'mailer'
 end
 
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,16 @@
 class UserMailer < ApplicationMailer
-  default from: "from@example.com"
+  default from: "jenyferpham@gmail.com"
 
   def contact_form(email, name, message)
   @message = message
-    mail(from: email,
-      to: 'your-email@example.com',
-      subject: "A new contact form message from #{name}")
+    mail(:from => email,
+         :to => 'jenyferpham@gmail.com',
+         :subject => "A new contact form message from #{name}")
+  end
+  
+  def welcome(user)
+    @appname = "Bike Berlin"
+    mail(:to => user.email, 
+         :subject => "Welcome to #{@appname}!")
   end
 end
