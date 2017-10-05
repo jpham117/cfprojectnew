3a049f0bf92b31a7544bf1c61634556b73f04dde
Jenyfer <jenyferpham@gmail.com>
2017-10-05 03:12:37 +0000

    rspec fixed for users_controller_spec.rb & product_spec.rb

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe UsersController, type: :controller do
   before do
     @user = FactoryGirl.create(:user)
-    let(:user2) { User.create!(email: "user2@gmail.com", password: "user2user2")}
+    @user2 = FactoryGirl.create(:user)
   end
   
   describe 'GET#show' do
@@ -12,12 +12,12 @@ describe UsersController, type: :controller do
         sign_in @user
       end
       it 'loads correct user details' do
-        get :show, params: { id: user.id }
+        get :show, params: { id: @user.id }
         expect(assigns(:user)).to eq @user
         expect(response).to have_http_status(200)
       end
       it 'cant access other users show page' do
-        get :show, params: { id: user2.id }
+        get :show, params: { id: @user2.id }
         expect(response).to have_http_status(302)
         expect(response).to redirect_to(root_path)
       end
@@ -25,7 +25,7 @@ describe UsersController, type: :controller do
     
     context 'when a user is not logged in' do
       it 'redirects to login' do
-      get :show, params: { id: user.id }
+      get :show, params: { id: @user.id }
       expect(response).to redirect_to(new_user_session_path)
       end
     end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'  # To get access to the configuration we need to write RSpec tests
 
 describe Product do  # We add the describe block to tell RSpec which model we want to test. 
-  context "when the product has comments" do
+   context "when the product has comments" do
+     let(:product) { Product.create!(name: "race bike") }
+     let(:user) { User.create!(first_name: "Jen", last_name:"Pham", email: "jenpham@gmail.com", password: "test123")}
+    
     before do
       Product.create!(name: "race bike")
       User.create!(email: "test1@gmail.com", password: "testtest") 
@@ -11,7 +14,7 @@ describe Product do  # We add the describe block to tell RSpec which model we wa
     end
   
     it "returns the average rating of all comments" do
-      expect(@product.average_rating).to eq 3
+      expect(product.average_rating).to eq 3
     end
     
     it "not valid without a name" do
